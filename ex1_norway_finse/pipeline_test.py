c55958691a66c74ee2471bfbb7f1e80963ff9c1a
arcticsnow <sfilhol@protonmail.com>
2023-05-12 16:09:54 +0200

    added pipeline_test.py

---
+++ ex1_norway_finse/pipeline_test.py
@@ -0,0 +1,19 @@
+from TopoPyScale import topoclass as tc
+
+#------------------------------------------
+print('\n ------------------------------- \n')
+print('Toposub Spatial Downscaling Example')
+print('\n ------------------------------- \n')
+
+config_file = '/config_test.yml'
+mp = tc.Topoclass(config_file)
+
+mp.compute_dem_param()
+mp.extract_topo_param()
+mp.compute_solar_geometry()
+mp.compute_horizon()
+mp.downscale_climate()
+print('---> Test pass for spatial (toposub) downscaling')
+
+
+
